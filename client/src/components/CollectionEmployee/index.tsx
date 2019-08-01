1164f047f7fb4bb903d3e0a97272b8b7c81275f7
J0315C-human <foibles@gmail.com>
2019-08-01 16:41:20 -0500

    add react-spring and transition for CollectionEmployee

--- client/src/components/CollectionEmployee/index.tsx
+++ client/src/components/CollectionEmployee/index.tsx
@@ -2,16 +2,29 @@ import * as React from 'react';
 import CardEmployee from '../CardEmployee';
 import { Employee } from '../../typings/api';
 import { Grid } from '@material-ui/core';
+import { useState } from 'react';
+import { useTransition, animated } from 'react-spring';
 
 interface CollectionEmployeeProps {
   employees: Employee[];
 }
 
 const CollectionEmployee: React.FunctionComponent<CollectionEmployeeProps> = props => {
+  const [items, set] = useState(props.employees);
+
+  const transitions = useTransition(items, item => item.id, {
+    from: { transform: 'translate3d(0,-20px,0)', opacity: 0 },
+    enter: { transform: 'translate3d(0,0px,0)', opacity: 1 },
+    leave: { transform: 'translate3d(0,-20px,0)', opacity: 0 },
+    trail: 35,
+  });
+
   return (
     <Grid container justify="center">
-      {props.employees.map((employee, i) => (
-        <CardEmployee key={i} employee={employee} />
+      {transitions.map(({ item, props, key }) => (
+        <animated.div key={key} style={props}>
+          <CardEmployee employee={item} />
+        </animated.div>
       ))}
     </Grid>
   );
